96ff62544db41842bba7f9d22b91dd1d3006c6f2
Rik Blok <rik.blok@ubc.ca>
2016-10-01 10:57:41 -0700

    fix links

--- makeReadme.cmd
+++ makeReadme.cmd
@@ -2,5 +2,5 @@
 
 echo # NetLogo-models > README.md
 echo. >> README.md
-for %%i in (*.nlogo) do echo * [http://netlogoweb.org/web?https://raw.githubusercontent.com/rikblok/NetLogo-models/master/%%~nxi](%%~nxi) >> README.md
+for %%i in (*.nlogo) do echo * [%%~nxi](http://netlogoweb.org/web?https://raw.githubusercontent.com/rikblok/NetLogo-models/master/%%~nxi) >> README.md
 :: To run brown2009 use this link: http://netlogoweb.org/web?https://raw.githubusercontent.com/rikblok/NetLogo-models/master/brown2009-web.nlogo
